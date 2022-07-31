f50da98d8199b8aa2fe85c99506029820bc112a5
Brian Durand <briandurand1990@gmail.com>
2022-07-31 01:07:46 -0300

    [Update] RoadToV1 - Miscellaneous

---
+++ pages/changelog.jsx
@@ -0,0 +1,11 @@
+import CustomHead from "../components/Head";
+
+export default function Home() {
+    return (
+        <>
+            <CustomHead title="Brian Durand | Full Stack Developer" index follow />
+            <h1>Changelog</h1>
+        </>
+    )
+}
+
