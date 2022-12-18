8405beaccc3b71e8661c0525f8cc90a749d0f53f
alfacoy <briandurand1990@gmail.com>
2022-12-18 13:48:32 -0300

    [Updated] Se cambia de posición el inicio de Analytic y se agrega el changelog.

--- components/Container/index.jsx
+++ components/Container/index.jsx
@@ -1,42 +1,24 @@
 import { useState } from 'react';
-import  Script from 'next/script';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
 import CustomHead from '../CustomHead';
 import ThemeContext from '../Theme';
 import style from './style.module.css';
 
-
 const Container = ({ children }) => {
     const [theme,setTheme] = useState('dark');
 
-    return (
-        <>
-            <Script src={`https://www.googletagmanager.com/gtag/js?id=G-HG9L20SGD6`} />
-            <Script
-             id="google-analytics"
-             dangerouslySetInnerHTML={{
-                __html:`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-HG9L20SGD6', {
-                        page_path: window.location.pathname,
-                    });
-                `
-             }}
-             />
-            <ThemeContext.Provider value={theme}>
-                <CustomHead title="Brian Durand | Full Stack Developer" index follow/>
-                <div className={`${style.container} ${theme}`}>
-                    <Navbar themeController={setTheme}/>
-                    <main className={`${style.main}`}>
-                        {children}
-                    </main>
-                    <Footer />
-                </div>
-            </ThemeContext.Provider>
-        </>
+    return (  
+        <ThemeContext.Provider value={theme}>
+            <CustomHead title="Brian Durand | Full Stack Developer" index follow/>
+            <div className={`${style.container} ${theme}`}>
+                <Navbar themeController={setTheme}/>
+                <main className={`${style.main}`}>
+                    {children}
+                </main>
+                <Footer />
+            </div>
+        </ThemeContext.Provider>     
     )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,26 @@
+import  Script from 'next/script';
 import Container from "../components/Container";
-
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps}) {
   return (
-      <Container>
-        <Component {...pageProps} />
-      </Container>
+      <>
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=G-HG9L20SGD6`} />
+        <Script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html:`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-HG9L20SGD6', {
+                    page_path: window.location.pathname,
+                });
+            `}}/>
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </>
   )
 }
 
