380f125def0de4730f1bd82d7c68f46128e0b23b
alfacoy <briandurand1990@gmail.com>
2022-12-18 13:24:21 -0300

    [Updated] Se hace una primera aproximación al analytic de google.

--- config.js
+++ config.js
@@ -6,6 +6,9 @@ const config = {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
         to: process.env.EMAIL_TO,
+    }, 
+    google: {
+        tracking: process.env.GOOGLE_ANALYTICS
     }
 }
 
